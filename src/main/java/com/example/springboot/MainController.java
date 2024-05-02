3d2aadc1fa7149fc33b161a195cdbe18a7995756
HarishDemoGit <harishdurai2@gmail.com>
2024-05-02 11:50:07 +0530

    test1

--- src/main/java/com/example/springboot/MainController.java
+++ src/main/java/com/example/springboot/MainController.java
@@ -1,13 +1,18 @@
 package com.example.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/eks2")
 public class MainController {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/user")
     public String sample() {
@@ -17,5 +22,11 @@ public class MainController {
     @PostMapping("/post")
     public String sample_01(String name) {
         return "hi I AM EKS2";
+    }
+
+    @GetMapping("/call-eks1")
+    public String callEks1() {
+        String url = "http://eks1-service.default.svc.cluster.local:8085/eks1/user";
+        return restTemplate.getForObject(url, String.class);
     }
 }
